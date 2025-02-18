e529fba24ffcd20ccc8307362dfe0ff3a98da648
Glitch (husky-dent-avatar) <none>
2025-02-18 16:28:26 +0000

    👘🏁 Checkpoint ./constants.js:74232600/22 ./clickonbox.js:74232600/229 ./index.html:74232600/7181

---
+++ clickonbox.js
@@ -0,0 +1,12 @@
+/* global AFRAME */
+
+AFRAME.registerComponent('click-on-box', {
+
+  init: function () {
+    this.el.addEventListener('click', this.boxClicked.bind(this));
+  },
+
+  boxClicked: function() {
+    console.log('I was clicked');
+  }
+});
--- constants.js
+++ constants.js
@@ -1,3 +1,3 @@
 const BOX_SPEED = 4
-const BOX_NOT_SELECTED_COLOR = 'blue';
-const BOX_SELECTED_COLOR = 'yellow';
+const BOX_NOT_SELECTED_COLOR = 'white';
+const BOX_SELECTED_COLOR = 'red';
